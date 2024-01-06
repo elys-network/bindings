e2bfe84a63b9a69b262d386361a729151945b29c
Cosmic Vagabond <121588426+cosmic-vagabond@users.noreply.github.com>
2024-01-06 08:31:50 +0100

    fix: get-pod-liq-asset(s) to include optional address (#57)

--- contracts/financial-snapshot-contract/src/action/query/pod/get_pod_liquid_asset.rs
+++ contracts/financial-snapshot-contract/src/action/query/pod/get_pod_liquid_asset.rs
@@ -1,9 +1,15 @@
 use super::*;
 use crate::{bindings::query::ElysQuery, msg::query_resp::pod::GetLiquidAssetResp};
 
-pub fn get_pod_liquid_asset(deps: Deps<ElysQuery>, asset: String) -> Result<GetLiquidAssetResp, ContractError> {
+pub fn get_pod_liquid_asset(
+    deps: Deps<ElysQuery>,
+    asset: String,
+    _address: Option<String>,
+) -> Result<GetLiquidAssetResp, ContractError> {
     let liquid_assets: Vec<LiquidAsset> = LIQUID_ASSETS.load(deps.storage)?;
-    let have_assets: Option<&LiquidAsset> = liquid_assets.iter().find(|liquid_asset| liquid_asset.asset == asset);
+    let have_assets: Option<&LiquidAsset> = liquid_assets
+        .iter()
+        .find(|liquid_asset| liquid_asset.asset == asset);
     let resp: GetLiquidAssetResp = GetLiquidAssetResp {
         liquid_asset: match have_assets {
             Some(liquid_asset) => liquid_asset.to_owned(),
