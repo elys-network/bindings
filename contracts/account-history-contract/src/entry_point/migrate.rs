03fa9c1308f4479c14ff80dd49622195614791e5
politeWall <138504353+politeWall@users.noreply.github.com>
2024-01-12 07:54:56 +0100

    ci: add value denom

--- contracts/account-history-contract/src/entry_point/migrate.rs
+++ contracts/account-history-contract/src/entry_point/migrate.rs
@@ -1,6 +1,6 @@
 use cosmwasm_std::Empty;
 
-use crate::states::TRADE_SHIELD_ADDRESS;
+use crate::states::{TRADE_SHIELD_ADDRESS, VALUE_DENOM};
 
 use super::*;
 
@@ -9,6 +9,10 @@ pub fn migrate(deps: DepsMut<ElysQuery>, _env: Env, _msg: Empty) -> StdResult<Re
     TRADE_SHIELD_ADDRESS.save(
         deps.storage,
         &"elys1m3hduhk4uzxn8mxuvpz02ysndxfwgy5mq60h4c34qqn67xud584qeee3m4".to_string(),
+    )?;
+    VALUE_DENOM.save(
+        deps.storage,
+        &"ibc/2180E84E20F5679FCC760D8C165B60F42065DEF7F46A72B447CFF1B7DC6C0A65".to_string(),
     )?;
     Ok(Response::new())
 }
