cb16e0fda2910a31dad5770c353f35f74f7389ae
kenta-elys <kenta@elys.network>
2024-01-17 16:04:45 +0000

    chore: add logic for cleaning history in migration

--- contracts/account-history-contract/src/entry_point/migrate.rs
+++ contracts/account-history-contract/src/entry_point/migrate.rs
@@ -1,8 +1,18 @@
-use cosmwasm_std::Empty;
+use cosmwasm_std::{Empty, StdError};
 
 use super::*;
+use crate::states::HISTORY;
+
 
 #[cfg_attr(not(feature = "library"), entry_point)]
-pub fn migrate(_deps: DepsMut<ElysQuery>, _env: Env, _msg: Empty) -> StdResult<Response<ElysMsg>> {
+pub fn migrate(deps: DepsMut<ElysQuery>, _env: Env, _msg: Empty) -> StdResult<Response<ElysMsg>> {
+    let keys: Vec<String> = HISTORY
+        .keys(deps.storage, None, None, cosmwasm_std::Order::Ascending)
+        .map(|key_result| key_result.map(|key| key))
+        .collect::<Result<Vec<String>, StdError>>()?;
+
+    for key in keys {
+        HISTORY.remove(deps.storage, key.as_str());
+    }
     Ok(Response::new())
 }
