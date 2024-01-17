b1256ae3bce7790d48596c1996d73e909c814633
politeWall <138504353+politeWall@users.noreply.github.com>
2024-01-17 08:31:33 +0100

    feat: no execution endpoint / migration cleaned (#79)
    feat: no execution enpoint / migration cleaned

--- contracts/account-history-contract/src/entry_point/execute.rs
+++ contracts/account-history-contract/src/entry_point/execute.rs
@@ -1,22 +1,13 @@
+use cosmwasm_std::Empty;
+
 use super::*;
-use crate::{action::sudo::update_account, msg::ExecuteMsg, states::PAGINATION};
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn execute(
-    deps: DepsMut<ElysQuery>,
-    env: Env,
+    _deps: DepsMut<ElysQuery>,
+    _env: Env,
     _info: MessageInfo,
-    msg: ExecuteMsg,
+    _msg: Empty,
 ) -> StdResult<Response<ElysMsg>> {
-    use ExecuteMsg::*;
-
-    match msg {
-        SetLimit { limit } => {
-            let mut p = PAGINATION.load(deps.storage)?;
-            p.limit = limit;
-            PAGINATION.save(deps.storage, &p)?;
-            Ok(Response::new())
-        }
-        UpdateAccount {} => update_account(deps, env),
-    }
+    Ok(Response::new())
 }
--- contracts/account-history-contract/src/entry_point/migrate.rs
+++ contracts/account-history-contract/src/entry_point/migrate.rs
@@ -1,18 +1,8 @@
 use cosmwasm_std::Empty;
 
-use crate::states::{TRADE_SHIELD_ADDRESS, VALUE_DENOM};
-
 use super::*;
 
 #[cfg_attr(not(feature = "library"), entry_point)]
-pub fn migrate(deps: DepsMut<ElysQuery>, _env: Env, _msg: Empty) -> StdResult<Response<ElysMsg>> {
-    TRADE_SHIELD_ADDRESS.save(
-        deps.storage,
-        &"elys1m3hduhk4uzxn8mxuvpz02ysndxfwgy5mq60h4c34qqn67xud584qeee3m4".to_string(),
-    )?;
-    VALUE_DENOM.save(
-        deps.storage,
-        &"ibc/2180E84E20F5679FCC760D8C165B60F42065DEF7F46A72B447CFF1B7DC6C0A65".to_string(),
-    )?;
+pub fn migrate(_deps: DepsMut<ElysQuery>, _env: Env, _msg: Empty) -> StdResult<Response<ElysMsg>> {
     Ok(Response::new())
 }
