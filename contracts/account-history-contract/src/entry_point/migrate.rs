d691fd84519e47b8c761c2afbc38342a91cb20bf
Cosmic Vagabond <121588426+cosmic-vagabond@users.noreply.github.com>
2024-01-18 06:45:11 +0100

    fix: use new state key for history to bypass conflicting structure

--- contracts/account-history-contract/src/entry_point/migrate.rs
+++ contracts/account-history-contract/src/entry_point/migrate.rs
@@ -1,33 +1,8 @@
-use cosmwasm_schema::cw_serde;
-use cosmwasm_std::{DecCoin, Empty, StdError};
-use cw_storage_plus::Map;
-use cw_utils::Expiration;
-
-use crate::types::CoinValue;
+use cosmwasm_std::Empty;
 
 use super::*;
 
-const OLD_HISTORY: Map<&str, Vec<OldAccountSnapshot>> = Map::new("history");
-
-#[cw_serde]
-struct OldAccountSnapshot {
-    pub date: Expiration,
-    pub total_liquid_asset_balance: DecCoin,
-    pub total_available_balance: DecCoin,
-    pub total_in_orders_balance: DecCoin,
-    pub available_asset_balance: Vec<CoinValue>,
-    pub in_orders_asset_balance: Vec<CoinValue>,
-}
-
 #[cfg_attr(not(feature = "library"), entry_point)]
-pub fn migrate(deps: DepsMut<ElysQuery>, _env: Env, _msg: Empty) -> StdResult<Response<ElysMsg>> {
-    let keys: Vec<String> = OLD_HISTORY
-        .keys(deps.storage, None, None, cosmwasm_std::Order::Ascending)
-        .map(|key_result| key_result.map(|key| key))
-        .collect::<Result<Vec<String>, StdError>>()?;
-
-    for key in keys {
-        OLD_HISTORY.remove(deps.storage, key.as_str());
-    }
+pub fn migrate(_deps: DepsMut<ElysQuery>, _env: Env, _msg: Empty) -> StdResult<Response<ElysMsg>> {
     Ok(Response::new())
 }
