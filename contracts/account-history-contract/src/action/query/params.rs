2f128db54b0434d4ec8cecef7e66615dace25fc2
Cosmic Vagabond <121588426+cosmic-vagabond@users.noreply.github.com>
2024-01-22 04:50:24 +0100

    fix: params (#109)

--- contracts/account-history-contract/src/action/query/params.rs
+++ contracts/account-history-contract/src/action/query/params.rs
@@ -1,5 +1,5 @@
 use crate::{
-    action::{EXPIRATION, PAGINATION, VALUE_DENOM},
+    action::{EXPIRATION, PAGINATION, TRADE_SHIELD_ADDRESS, VALUE_DENOM},
     msg::query_resp::ParamsResp,
 };
 use cosmwasm_std::{Deps, StdResult};
@@ -9,10 +9,12 @@ pub fn params(deps: Deps<ElysQuery>) -> StdResult<ParamsResp> {
     let value_denom = VALUE_DENOM.load(deps.storage)?;
     let expiration = EXPIRATION.load(deps.storage)?;
     let pagination = PAGINATION.load(deps.storage)?;
+    let trade_shield_address = TRADE_SHIELD_ADDRESS.load(deps.storage)?;
 
     Ok(ParamsResp {
         expiration,
         pagination,
         value_denom,
+        trade_shield_address,
     })
 }
