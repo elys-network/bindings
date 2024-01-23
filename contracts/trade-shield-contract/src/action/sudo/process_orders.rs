1c764405c5850b1bd8d1bb9f07fa20b95b89a14e
politeWall <138504353+politeWall@users.noreply.github.com>
2024-01-23 08:04:59 +0100

    fix: check-spot-order

--- contracts/trade-shield-contract/src/action/sudo/process_orders.rs
+++ contracts/trade-shield-contract/src/action/sudo/process_orders.rs
@@ -192,14 +192,14 @@ fn check_spot_order(
         false => Decimal::one().div(order.order_price.rate),
     };
 
-    let order_token_out = order_spot_price * order.order_amount.amount;
+    let market_price = amm_swap_estimation.spot_price;
 
     match order.order_type {
-        SpotOrderType::LimitBuy => order_token_out <= amm_swap_estimation.amount.amount,
+        SpotOrderType::LimitBuy => market_price <= order_spot_price,
 
-        SpotOrderType::LimitSell => order_token_out <= amm_swap_estimation.amount.amount,
+        SpotOrderType::LimitSell => market_price >= order_spot_price,
 
-        SpotOrderType::StopLoss => order_token_out >= amm_swap_estimation.amount.amount,
+        SpotOrderType::StopLoss => market_price <= order_spot_price,
         _ => false,
     }
 }
@@ -267,4 +267,95 @@ fn calculate_token_out_min_amount(order: &SpotOrder) -> Int128 {
     };
 
     Int128::new((amount.u128()) as i128)
+}
+
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+
+    use cosmwasm_std::{coin, Addr, SignedDecimal, Timestamp};
+
+    use super::*;
+
+    #[test]
+    fn test_check_spot_order_limit_buy() {
+        // Arrange
+        let spot_order = SpotOrder {
+            order_type: SpotOrderType::LimitBuy,
+            order_id: 1,
+            order_price: OrderPrice {
+                base_denom: "uatom".to_string(),
+                quote_denom: "usdc".to_string(),
+                rate: Decimal::from_str("9.0").unwrap(),
+            },
+            order_amount: coin(1000000, "usdc"),
+
+            owner_address: Addr::unchecked("elysd"),
+            order_target_denom: "uatom".to_string(),
+            status: Status::Pending,
+            date: Date {
+                height: 5,
+                time: Timestamp::from_seconds(5),
+            },
+            // Initialize the rest of the SpotOrder fields here
+        };
+        let amm_swap_estimation = AmmSwapEstimationByDenomResponse {
+            spot_price: Decimal::from_str("9.29").unwrap(),
+            in_route: None,
+            out_route: None,
+            amount: coin(1000000, "uatom"),
+            swap_fee: SignedDecimal::zero(),
+            discount: SignedDecimal::zero(),
+            available_liquidity: coin(1000000, "uatom"),
+            weight_balance_ratio: SignedDecimal::zero(),
+            price_impact: SignedDecimal::zero(),
+        };
+
+        // Act
+        let result = check_spot_order(&spot_order, &amm_swap_estimation);
+
+        // Assert
+        assert_eq!(result, false); // Change as needed
+    }
+
+    #[test]
+    fn test_check_spot_order_limit_sell() {
+        // Arrange
+        let spot_order = SpotOrder {
+            order_type: SpotOrderType::LimitSell,
+            order_id: 1,
+            order_price: OrderPrice {
+                base_denom: "uatom".to_string(),
+                quote_denom: "usdc".to_string(),
+                rate: Decimal::from_str("10.0").unwrap(),
+            },
+            order_amount: coin(1000000, "uatom"),
+
+            owner_address: Addr::unchecked("elysd"),
+            order_target_denom: "usdc".to_string(),
+            status: Status::Pending,
+            date: Date {
+                height: 5,
+                time: Timestamp::from_seconds(5),
+            },
+            // Initialize the rest of the SpotOrder fields here
+        };
+        let amm_swap_estimation = AmmSwapEstimationByDenomResponse {
+            spot_price: Decimal::from_str("9.29").unwrap(),
+            in_route: None,
+            out_route: None,
+            amount: coin(1000000, "usdc"),
+            swap_fee: SignedDecimal::zero(),
+            discount: SignedDecimal::zero(),
+            available_liquidity: coin(1000000, "usdc"),
+            weight_balance_ratio: SignedDecimal::zero(),
+            price_impact: SignedDecimal::zero(),
+        };
+
+        // Act
+        let result = check_spot_order(&spot_order, &amm_swap_estimation);
+
+        // Assert
+        assert_eq!(result, false); // Change as needed
+    }
 }
