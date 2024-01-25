ceb3b79a4a815a32c5c5ef6f6e09d1d05837c03c
politeWall <138504353+politeWall@users.noreply.github.com>
2024-01-25 07:27:22 +0100

    revert: change in test_history.sh

--- scripts/test_history.sh
+++ scripts/test_history.sh
@@ -6,10 +6,10 @@ extract_contract_address() { awk -F 'key: _contract_address|value: ' '/key: _con
 
 OPTIONS="--from validator --gas auto --gas-adjustment=1.3 --fees 100000uelys -b sync -y"
 
-# docker run --rm -v "$(pwd)":/code \
-#           --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
-#           --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
-#           cosmwasm/workspace-optimizer:0.14.0
+docker run --rm -v "$(pwd)":/code \
+          --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
+          --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
+          cosmwasm/workspace-optimizer:0.14.0
 
 # store and init trade shield contract
 txhash=$(elysd tx wasm store artifacts/trade_shield_contract.wasm $OPTIONS | extract_txhash)
@@ -22,15 +22,15 @@ addr=$(elysd q tx $txhash | extract_contract_address)
 echo tradeshield : $addr
 
 
-# # store and init account history contract
-# txhash=$(elysd tx wasm store artifacts/account_history_contract.wasm $OPTIONS | extract_txhash)
-# sleep 10
-# codeid=$(elysd q tx $txhash | extract_code_id)
-# msg=$(echo '{"limit" : 50, "expiration": {"at_time":"604800000000000"}, "value_denom" : "ibc/2180E84E20F5679FCC760D8C165B60F42065DEF7F46A72B447CFF1B7DC6C0A65", "trade_shield_address" :"'$addr'"}')
-# txhash=$(elysd tx wasm init $codeid "$msg" $OPTIONS --label "Contract" --admin validator | extract_txhash)
-# sleep 10
-# addr=$(elysd q tx $txhash | extract_contract_address)
-# echo history : $addr
-# elysd tx wasm exec $addr '{"update_account" : {}}' --from validator --gas-prices 0.25uelys --gas auto --gas-adjustment 1.3 -b sync -y  --keyring-backend=test --chain-id=elystestnet-1
-# elysd q wasm contract-state smart $addr2 '{"all" : {}}'
-# # elysd q wasm contract-state smart $addr2 '{"get_liquid_assets" : {"user_address" : "WRITE THE USER ADDRESS"}}'
+# store and init account history contract
+txhash=$(elysd tx wasm store artifacts/account_history_contract.wasm $OPTIONS | extract_txhash)
+sleep 10
+codeid=$(elysd q tx $txhash | extract_code_id)
+msg=$(echo '{"limit" : 50, "expiration": {"at_time":"604800000000000"}, "value_denom" : "ibc/2180E84E20F5679FCC760D8C165B60F42065DEF7F46A72B447CFF1B7DC6C0A65", "trade_shield_address" :"'$addr'"}')
+txhash=$(elysd tx wasm init $codeid "$msg" $OPTIONS --label "Contract" --admin validator | extract_txhash)
+sleep 10
+addr=$(elysd q tx $txhash | extract_contract_address)
+echo history : $addr
+elysd tx wasm exec $addr '{"update_account" : {}}' --from validator --gas-prices 0.25uelys --gas auto --gas-adjustment 1.3 -b sync -y  --keyring-backend=test --chain-id=elystestnet-1
+elysd q wasm contract-state smart $addr2 '{"all" : {}}'
+# elysd q wasm contract-state smart $addr2 '{"get_liquid_assets" : {"user_address" : "WRITE THE USER ADDRESS"}}'
