987b6139007fa234a34043a5f5e19c3e09441545
Cosmic Vagabond <121588426+cosmic-vagabond@users.noreply.github.com>
2024-02-03 06:50:02 +0100

    fix: balance 24h

--- contracts/account-history-contract/src/msg/query_resp/get_portfolio_resp.rs
+++ contracts/account-history-contract/src/msg/query_resp/get_portfolio_resp.rs
@@ -6,5 +6,7 @@ use cosmwasm_std::SignedDecimal256;
 #[cw_serde]
 pub struct GetPortfolioResp {
     pub portfolio: Portfolio,
+    pub actual_portfolio_balance: SignedDecimal256,
+    pub old_portfolio_balance: SignedDecimal256,
     pub balance_24h_change: SignedDecimal256,
 }
