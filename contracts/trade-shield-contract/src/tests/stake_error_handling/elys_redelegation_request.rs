73d96432e508fa513c69aa4a539dd2c75a9dc692
politeWall <138504353+politeWall@users.noreply.github.com>
2024-02-26 05:45:20 +0100

    test: add tests for the the change in the PR #210 and fix messages

---
+++ contracts/trade-shield-contract/src/tests/stake_error_handling/elys_redelegation_request.rs
@@ -0,0 +1,88 @@
+use cosmwasm_std::StdError;
+
+use super::*;
+
+#[test]
+fn elys_redelegation_request_error() {
+    let mut app = ElysApp::new();
+
+    let instantiate_msg = InstantiateMockMsg {
+        account_history_address: None,
+        spot_orders: vec![],
+        perpetual_orders: vec![],
+    };
+
+    let code = ContractWrapper::new(execute, instantiate, query);
+    let code_id = app.store_code(Box::new(code));
+
+    let addr = app
+        .instantiate_contract(
+            code_id,
+            Addr::unchecked("owner"),
+            &instantiate_msg,
+            &[],
+            "Contract",
+            None,
+        )
+        .unwrap();
+
+    let msg = ExecuteMsg::ElysRedelegateRequest {
+        amount: coin(0, "uusdc"),
+
+        validator_src_address: "".to_string(),
+        validator_dst_address: "".to_string(),
+    };
+
+    let err = app
+        .execute_contract(Addr::unchecked("user"), addr.clone(), &msg, &[])
+        .unwrap_err();
+
+    assert_eq!(
+        ContractError::StdError(StdError::generic_err("amount is zero")),
+        err.downcast().unwrap()
+    );
+
+    let msg = ExecuteMsg::ElysRedelegateRequest {
+        amount: coin(10, "uelys"),
+        validator_src_address: "".to_string(),
+        validator_dst_address: "validator".to_string(),
+    };
+
+    let err = app
+        .execute_contract(Addr::unchecked("user"), addr.clone(), &msg, &[])
+        .unwrap_err();
+
+    assert_eq!(
+        ContractError::StdError(StdError::generic_err(
+            "The validator Address is required only if the staked asset is uelys"
+        )),
+        err.downcast().unwrap()
+    );
+
+    let msg = ExecuteMsg::ElysRedelegateRequest {
+        amount: coin(10, "uelys"),
+        validator_src_address: "validator".to_string(),
+        validator_dst_address: "".to_string(),
+    };
+
+    let err = app
+        .execute_contract(Addr::unchecked("user"), addr.clone(), &msg, &[])
+        .unwrap_err();
+
+    assert_eq!(
+        ContractError::StdError(StdError::generic_err(
+            "The validator Address is required only if the staked asset is uelys"
+        )),
+        err.downcast().unwrap()
+    );
+
+    let msg = ExecuteMsg::ElysRedelegateRequest {
+        amount: coin(10, "uelys"),
+        validator_src_address: "validator_src".to_string(),
+        validator_dst_address: "validator_dst".to_string(),
+    };
+
+    let resp = app.execute_contract(Addr::unchecked("user"), addr.clone(), &msg, &[]);
+
+    assert!(resp.is_ok());
+}
