93ccd0ac5856d6fdedd4ad8e61e1b27ec9b458eb
politeWall <138504353+politeWall@users.noreply.github.com>
2024-04-06 01:29:54 +0200

    fix: process order error

--- contracts/trade-shield-contract/src/entry_point/migrate.rs
+++ contracts/trade-shield-contract/src/entry_point/migrate.rs
@@ -1,9 +1,10 @@
+use cosmwasm_std::Order;
 use elys_bindings::trade_shield::{
     msg::MigrateMsg,
     states::{
         ACCOUNT_HISTORY_ADDRESS, LEVERAGE_ENABLED, LIMIT_PROCESS_ORDER, MARKET_ORDER_ENABLED,
-        PARAMS_ADMIN, PERPETUAL_ENABLED, PROCESS_ORDERS_ENABLED, REWARD_ENABLED, STAKE_ENABLED,
-        SWAP_ENABLED,
+        PARAMS_ADMIN, PENDING_SPOT_ORDER, PERPETUAL_ENABLED, PROCESS_ORDERS_ENABLED,
+        REWARD_ENABLED, SORTED_PENDING_SPOT_ORDER, STAKE_ENABLED, SWAP_ENABLED,
     },
 };
 
@@ -32,6 +33,21 @@ pub fn migrate(
     REWARD_ENABLED.save(deps.storage, &state)?;
     LEVERAGE_ENABLED.save(deps.storage, &state)?;
     LIMIT_PROCESS_ORDER.save(deps.storage, &limit_process_order)?;
+
+    let spot_orders_ids: Vec<(String, Vec<u64>)> = SORTED_PENDING_SPOT_ORDER
+        .prefix_range(deps.storage, None, None, Order::Ascending)
+        .filter_map(|res| res.ok())
+        .collect();
+
+    for (key, list) in spot_orders_ids {
+        let mut existing: Vec<u64> = vec![];
+        for id in list {
+            if PENDING_SPOT_ORDER.may_load(deps.storage, id)?.is_some() {
+                existing.push(id);
+            }
+        }
+        SORTED_PENDING_SPOT_ORDER.save(deps.storage, key.as_str(), &existing)?;
+    }
 
     Ok(Response::new())
 }
