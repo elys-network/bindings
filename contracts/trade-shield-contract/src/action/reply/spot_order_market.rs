eea7b5645f20e55b040cc461a0c461bd3ea4539e
Cosmic Vagabond <121588426+cosmic-vagabond@users.noreply.github.com>
2024-04-23 15:57:24 +0200

    test: improve test endpoint (#292)

--- contracts/trade-shield-contract/src/action/reply/spot_order_market.rs
+++ contracts/trade-shield-contract/src/action/reply/spot_order_market.rs
@@ -1,6 +1,5 @@
-use cosmwasm_std::{from_json, Binary, DepsMut, StdError, SubMsgResult};
-
 use super::*;
+use cosmwasm_std::{from_json, Binary, DepsMut, StdError, SubMsgResult};
 
 pub fn reply_to_spot_order_market(
     deps: DepsMut<ElysQuery>,
@@ -8,19 +7,28 @@ pub fn reply_to_spot_order_market(
     module_resp: SubMsgResult,
 ) -> Result<Response<ElysMsg>, ContractError> {
     if let Err(err) = module_resp.into_result() {
-        return Err(StdError::generic_err(err).into());
+        return Err(StdError::generic_err(format!("{}: {}: {}", file!(), line!(), err)).into());
     };
 
     let order_id: u64 = match data {
-        Some(order_id) => from_json(&order_id)?,
-        None => return Err(StdError::generic_err("no meta_data".to_string()).into()),
+        Some(order_id) => from_json(&order_id)
+            .map_err(|e| StdError::generic_err(format!("{}: {}: {}", file!(), line!(), e)))?,
+        None => {
+            return Err(
+                StdError::generic_err(format!("{}: {}: no meta_data", file!(), line!())).into(),
+            )
+        }
     };
 
-    let mut order = SPOT_ORDER.load(deps.storage, order_id)?;
+    let mut order = SPOT_ORDER
+        .load(deps.storage, order_id)
+        .map_err(|e| StdError::generic_err(format!("{}: {}: {}", file!(), line!(), e)))?;
 
     order.status = Status::Executed;
 
-    SPOT_ORDER.save(deps.storage, order_id, &order)?;
+    SPOT_ORDER
+        .save(deps.storage, order_id, &order)
+        .map_err(|e| StdError::generic_err(format!("{}: {}: {}", file!(), line!(), e)))?;
 
     let resp: Response<ElysMsg> = Response::new().add_event(
         Event::new("reply_to_spot_order_market").add_attribute("order_id", order_id.to_string()),
