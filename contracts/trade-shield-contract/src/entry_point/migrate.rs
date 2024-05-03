83d1580cbf3874dd7466ac95dc7aba9c91458fcc
politeWall <138504353+politeWall@users.noreply.github.com>
2024-05-03 17:26:00 +0200

    feat: 🎸 mark corrupted order as canceled

--- contracts/trade-shield-contract/src/entry_point/migrate.rs
+++ contracts/trade-shield-contract/src/entry_point/migrate.rs
@@ -1,14 +1,14 @@
-use cosmwasm_std::Order;
+use super::*;
+use cosmwasm_std::Storage;
 use elys_bindings::trade_shield::{
     msg::MigrateMsg,
     states::{
         ACCOUNT_HISTORY_ADDRESS, LEVERAGE_ENABLED, LIMIT_PROCESS_ORDER, MARKET_ORDER_ENABLED,
         PARAMS_ADMIN, PENDING_SPOT_ORDER, PERPETUAL_ENABLED, PROCESS_ORDERS_ENABLED,
-        REWARD_ENABLED, SORTED_PENDING_SPOT_ORDER, STAKE_ENABLED, SWAP_ENABLED,
+        REWARD_ENABLED, SORTED_PENDING_PERPETUAL_ORDER, SPOT_ORDER, STAKE_ENABLED, SWAP_ENABLED,
     },
+    types::{SpotOrder, Status},
 };
-
-use super::*;
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn migrate(
@@ -34,20 +34,51 @@ pub fn migrate(
     LEVERAGE_ENABLED.save(deps.storage, &state)?;
     LIMIT_PROCESS_ORDER.save(deps.storage, &limit_process_order)?;
 
-    let spot_orders_ids: Vec<(String, Vec<u64>)> = SORTED_PENDING_SPOT_ORDER
-        .prefix_range(deps.storage, None, None, Order::Ascending)
-        .filter_map(|res| res.ok())
+    let spot_orders: Vec<SpotOrder> = SPOT_ORDER
+        .prefix_range(deps.storage, None, None, cosmwasm_std::Order::Ascending)
+        .filter_map(|result| {
+            if let Ok((_, order)) = result {
+                if matches!(order.status, Status::Pending) {
+                    return Some(order);
+                }
+            }
+            None
+        })
         .collect();
 
-    for (key, list) in spot_orders_ids {
-        let mut existing: Vec<u64> = vec![];
-        for id in list {
-            if PENDING_SPOT_ORDER.may_load(deps.storage, id)?.is_some() {
-                existing.push(id);
-            }
+    for mut order in spot_orders {
+        let pending_order = PENDING_SPOT_ORDER.may_load(deps.storage, order.order_id)?;
+        if pending_order.is_none() {
+            cancel_order(&mut order, deps.storage)?;
+            continue;
         }
-        SORTED_PENDING_SPOT_ORDER.save(deps.storage, key.as_str(), &existing)?;
+
+        let key = order.gen_key()?;
+        if let Some(ids) = SORTED_PENDING_PERPETUAL_ORDER.may_load(deps.storage, key.as_str())? {
+            if let Some(&id) = ids.iter().find(|&&id| id == order.order_id) {
+                if pending_order.is_none() {
+                    let filtered_ids: Vec<u64> =
+                        ids.iter().filter(|&&id_| id_ != id).cloned().collect();
+                    SORTED_PENDING_PERPETUAL_ORDER.save(
+                        deps.storage,
+                        key.as_str(),
+                        &filtered_ids,
+                    )?;
+                }
+            } else {
+                cancel_order(&mut order, deps.storage)?;
+            }
+        } else {
+            cancel_order(&mut order, deps.storage)?;
+        }
     }
 
     Ok(Response::new())
+}
+
+fn cancel_order(order: &mut SpotOrder, storage: &mut dyn Storage) -> StdResult<()> {
+    order.status = Status::Canceled;
+    SPOT_ORDER.save(storage, order.order_id, order)?;
+    PENDING_SPOT_ORDER.remove(storage, order.order_id);
+    Ok(())
 }
