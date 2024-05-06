1483f5a2ff3805c76a7bc64a3fa07135607a8953
politeWall <138504353+politeWall@users.noreply.github.com>
2024-05-06 17:22:20 +0200

    feat: 🎸 starting including the element on the response

--- contracts/trade-shield-contract/src/action/query/get_stat.rs
+++ contracts/trade-shield-contract/src/action/query/get_stat.rs
@@ -3,11 +3,11 @@ use super::*;
 pub fn get_stat(deps: Deps<ElysQuery>, env: Env) -> Result<GetStatResponse, ContractError> {
     let querier = ElysQuerier::new(&deps.querier);
 
-    let _height = env.block.height;
+    let block_height = env.block.height;
     let AuthAddressesResponse {
         pagination:
             PageResponse {
-                total: _number_of_address_on_the_chain,
+                total: number_of_address_on_the_chain,
                 ..
             },
         ..
@@ -18,9 +18,19 @@ pub fn get_stat(deps: Deps<ElysQuery>, env: Env) -> Result<GetStatResponse, Cont
         count_total: true,
         reverse: true,
     }))?;
+    let number_of_address_on_the_chain = number_of_address_on_the_chain.unwrap_or(0);
 
-    let _number_of_pending_order = NUMBER_OF_PENDING_ORDER.load(deps.storage)?;
-    let _number_of_executed_order = NUMBER_OF_EXECUTED_ORDER.load(deps.storage)?;
+    let number_of_pending_order = NUMBER_OF_PENDING_ORDER.load(deps.storage)?;
+    let number_of_executed_order = NUMBER_OF_EXECUTED_ORDER.load(deps.storage)?;
 
-    unimplemented!()
+    let resp = GetStatResponse {
+        block_height,
+        number_of_address_on_the_chain,
+        number_of_executed_order,
+        number_of_pending_order,
+        number_of_address_in_commitment: 0,
+        number_of_address_in_incentive: 0,
+    };
+
+    Ok(resp)
 }
