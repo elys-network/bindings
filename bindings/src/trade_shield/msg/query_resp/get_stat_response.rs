c4b13f1ceb0f7274a1d11cabb1aebe92f695f172
politeWall <138504353+politeWall@users.noreply.github.com>
2024-05-16 18:00:38 +0200

    feat: 🎸 remove number of address

--- bindings/src/trade_shield/msg/query_resp/get_stat_response.rs
+++ bindings/src/trade_shield/msg/query_resp/get_stat_response.rs
@@ -3,7 +3,6 @@ use cosmwasm_schema::cw_serde;
 #[cw_serde]
 pub struct GetStatResponse {
     pub block_height: u64,
-    pub number_of_address_on_the_chain: u64,
     pub number_of_executed_order: u64,
     pub number_of_pending_order: u64,
     pub number_of_address_in_commitment: u64,
--- contracts/trade-shield-contract/src/action/query/get_stat.rs
+++ contracts/trade-shield-contract/src/action/query/get_stat.rs
@@ -4,21 +4,6 @@ pub fn get_stat(deps: Deps<ElysQuery>, env: Env) -> Result<GetStatResponse, Cont
     let querier = ElysQuerier::new(&deps.querier);
 
     let block_height = env.block.height;
-    let AuthAddressesResponse {
-        pagination:
-            PageResponse {
-                total: number_of_address_on_the_chain,
-                ..
-            },
-        ..
-    } = querier.accounts(Some(PageRequest {
-        key: None,
-        offset: None,
-        limit: 1,
-        count_total: true,
-        reverse: true,
-    }))?;
-    let number_of_address_on_the_chain = number_of_address_on_the_chain.unwrap_or(0);
 
     let number_of_pending_order = NUMBER_OF_PENDING_ORDER.load(deps.storage)?;
     let number_of_executed_order = NUMBER_OF_EXECUTED_ORDER.load(deps.storage)?;
@@ -28,7 +13,6 @@ pub fn get_stat(deps: Deps<ElysQuery>, env: Env) -> Result<GetStatResponse, Cont
 
     let resp = GetStatResponse {
         block_height,
-        number_of_address_on_the_chain,
         number_of_executed_order,
         number_of_pending_order,
         number_of_address_in_commitment,
