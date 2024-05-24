18303bda5acf3b9ecd520ea707ed90d34f57e69e
Amit Yadav <amy29981@gmail.com>
2024-05-24 07:41:12 +0530

    add version for migration

--- contracts/financial-snapshot-contract/src/entry_point/instantiate.rs
+++ contracts/financial-snapshot-contract/src/entry_point/instantiate.rs
@@ -3,12 +3,18 @@ use crate::states::*;
 use elys_bindings::ElysQuery;
 use msg::InstantiateMsg;
 
+use cw2::set_contract_version;
+
+const CONTRACT_NAME: &str = "financial-snapshot";
+const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
+
 pub fn instantiate(
     deps: DepsMut<ElysQuery>,
     _env: Env,
     _info: MessageInfo,
     _msg: InstantiateMsg,
 ) -> StdResult<Response> {
+    set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
     LIQUIDITY_POSITIONS.save(deps.storage, &vec![])?;
 
     Ok(Response::new())
