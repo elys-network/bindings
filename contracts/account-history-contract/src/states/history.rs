8016d8d35349e01dd47c68213adfc706d5aa6fb5
politeWall <138504353+politeWall@users.noreply.github.com>
2024-05-24 04:44:37 +0200

    Change namespace of ah `HISTORY` Map (#340)
    fix: 🐛 change namespace of ah hitorty Map

--- contracts/account-history-contract/src/states/history.rs
+++ contracts/account-history-contract/src/states/history.rs
@@ -4,4 +4,4 @@ use cw_storage_plus::Map;
 use elys_bindings::account_history::types::PortfolioBalanceSnapshot;
 
 pub const HISTORY: Map<&str, HashMap<String, PortfolioBalanceSnapshot>> =
-    Map::new("history_portfolio_balance_snapshot_1");
+    Map::new("history_portfolio_balance_snapshot_2");
