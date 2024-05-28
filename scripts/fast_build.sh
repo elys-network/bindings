bad7cabde84f49124414eea18d83a5e282e6da35
Amit Yadav <amy29981@gmail.com>
2024-05-28 08:58:20 +0530

    CI flag

--- scripts/fast_build.sh
+++ scripts/fast_build.sh
@@ -11,11 +11,14 @@ mkdir -p artifacts
 
 rm -rf ./target/wasm32-unknown-unknown/
 
-export VERSION=$(git describe --tags --match 'v*' --abbrev=0 | sed 's/^v//')
-
-sed -i '' "s/^version = .*/version = \"$VERSION\"/" contracts/account-history-contract/Cargo.toml
-sed -i '' "s/^version = .*/version = \"$VERSION\"/" contracts/trade-shield-contract/Cargo.toml
-sed -i '' "s/^version = .*/version = \"$VERSION\"/" contracts/financial-snapshot/Cargo.toml
+# Check if the CI environment variable is set to true
+if [ "${CI:-}" = "true" ]; then
+    echo "CI is true, running the block of commands..."
+    export VERSION=$(git describe --tags --match 'v*' --abbrev=0 | sed 's/^v//')
+    sed -i '' "s/^version = .*/version = \"$VERSION\"/" contracts/account-history-contract/Cargo.toml
+    sed -i '' "s/^version = .*/version = \"$VERSION\"/" contracts/trade-shield-contract/Cargo.toml
+    sed -i '' "s/^version = .*/version = \"$VERSION\"/" contracts/financial-snapshot/Cargo.toml
+fi
 
 export RUST_BACKTRACE=1
 
