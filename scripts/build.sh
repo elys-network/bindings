9489b932b0d2e8cb3796ac454849b40be10ce14e
Amit Yadav <amy29981@gmail.com>
2024-05-28 19:11:42 +0530

    ci update

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,7 +6,7 @@ if [ "${CI:-}" = "true" ]; then
     export VERSION=$(git describe --tags --match 'v*' --abbrev=0 | sed 's/^v//')
     sed -i '' "s/^version = .*/version = \"$VERSION\"/" contracts/account-history-contract/Cargo.toml
     sed -i '' "s/^version = .*/version = \"$VERSION\"/" contracts/trade-shield-contract/Cargo.toml
-    sed -i '' "s/^version = .*/version = \"$VERSION\"/" contracts/financial-snapshot/Cargo.toml
+    sed -i '' "s/^version = .*/version = \"$VERSION\"/" contracts/financial-snapshot-contract/Cargo.toml
 fi
 
 docker run --rm -v "$(pwd)":/code \
