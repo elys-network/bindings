fcbb9c4eb768ed68002f10bf5f71643a0e376a54
Amit Yadav <amy29981@gmail.com>
2024-05-28 19:21:52 +0530

    ci

--- scripts/fast_build.sh
+++ scripts/fast_build.sh
@@ -14,7 +14,9 @@ rm -rf ./target/wasm32-unknown-unknown/
 # Check if the CI environment variable is set to true
 if [ "${CI:-}" = "true" ]; then
     echo "CI is true, running the block of commands..."
+    git tag
     export VERSION=$(git describe --tags --match 'v*' --abbrev=0 | sed 's/^v//')
+    echo $VERSION
     sed -i "s/^version = .*/version = \"$VERSION\"/" contracts/account-history-contract/Cargo.toml
     sed -i "s/^version = .*/version = \"$VERSION\"/" contracts/trade-shield-contract/Cargo.toml
     sed -i "s/^version = .*/version = \"$VERSION\"/" contracts/financial-snapshot-contract/Cargo.toml
