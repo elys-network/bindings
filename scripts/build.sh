dfc436eae54262f67b02d2fb970177103d838ef9
Amit Yadav <amy29981@gmail.com>
2024-05-28 19:31:20 +0530

    ci

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,9 +3,7 @@
 # Check if the CI environment variable is set to true
 if [ "${CI:-}" = "true" ]; then
     echo "CI is true, running the block of commands..."
-    git fetch --tags
     export VERSION=$(git describe --tags --match 'v*' --abbrev=0 | sed 's/^v//')
-    echo "${VERSION}"
     sed -i "s/^version = .*/version = \"$VERSION\"/" contracts/account-history-contract/Cargo.toml
     sed -i "s/^version = .*/version = \"$VERSION\"/" contracts/trade-shield-contract/Cargo.toml
     sed -i "s/^version = .*/version = \"$VERSION\"/" contracts/financial-snapshot-contract/Cargo.toml
