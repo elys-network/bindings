3e7bc2c0cfce850f9337281694dba87dc88fe8f3
Amit Yadav <amy29981@gmail.com>
2024-05-28 20:44:42 +0530

    Fix: Release CI (#354)
    * ci update
    
    * test
    
    * fix
    
    * ci
    
    * ci
    
    * ci
    
    * ci
    
    * ci

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,9 +4,9 @@
 if [ "${CI:-}" = "true" ]; then
     echo "CI is true, running the block of commands..."
     export VERSION=$(git describe --tags --match 'v*' --abbrev=0 | sed 's/^v//')
-    sed -i '' "s/^version = .*/version = \"$VERSION\"/" contracts/account-history-contract/Cargo.toml
-    sed -i '' "s/^version = .*/version = \"$VERSION\"/" contracts/trade-shield-contract/Cargo.toml
-    sed -i '' "s/^version = .*/version = \"$VERSION\"/" contracts/financial-snapshot/Cargo.toml
+    sed -i "s/^version = .*/version = \"$VERSION\"/" contracts/account-history-contract/Cargo.toml
+    sed -i "s/^version = .*/version = \"$VERSION\"/" contracts/trade-shield-contract/Cargo.toml
+    sed -i "s/^version = .*/version = \"$VERSION\"/" contracts/financial-snapshot-contract/Cargo.toml
 fi
 
 docker run --rm -v "$(pwd)":/code \
