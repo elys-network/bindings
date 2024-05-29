a5b30d53ea270ac0954bda7d4adddf8b0e80e530
Amit Yadav <amy29981@gmail.com>
2024-05-29 10:55:47 +0530

    add feature flag, call function from sudo function

--- contracts/account-history-contract/src/entry_point/sudo.rs
+++ contracts/account-history-contract/src/entry_point/sudo.rs
@@ -1,5 +1,9 @@
 use crate::msg::SudoMsg;
-use crate::{action::sudo::update_account, states::UPDATE_ACCOUNT_ENABLED};
+use crate::states::DELETE_OLD_DATA_ENABLED;
+use crate::{
+    action::sudo::{clean_old_history, clean_up_history, update_account},
+    states::UPDATE_ACCOUNT_ENABLED,
+};
 use cosmwasm_std::{entry_point, DepsMut, Env, Response, StdError, StdResult};
 use elys_bindings::{ElysMsg, ElysQuery};
 
@@ -10,6 +14,11 @@ pub fn sudo(deps: DepsMut<ElysQuery>, env: Env, msg: SudoMsg) -> StdResult<Respo
             if UPDATE_ACCOUNT_ENABLED.load(deps.storage)? == false {
                 return Err(StdError::generic_err("Update account is disabled"));
             }
+            if DELETE_OLD_DATA_ENABLED.load(deps.storage)? == true {
+                clean_old_history(deps, 1000u64)?;
+                clean_up_history(deps, env, 1000u64)?;
+            }
+
             update_account(deps, env)
         }
     }
--- contracts/account-history-contract/src/states/history.rs
+++ contracts/account-history-contract/src/states/history.rs
@@ -1,4 +1,4 @@
-use cw_storage_plus::Map;
+use cw_storage_plus::{Item, Map};
 use elys_bindings::account_history::types::PortfolioBalanceSnapshot;
 
 pub const OLD_HISTORY_1: Map<&str, PortfolioBalanceSnapshot> =
@@ -9,3 +9,5 @@ pub const OLD_HISTORY_2: Map<&str, PortfolioBalanceSnapshot> =
 
 pub const HISTORY: Map<&str, PortfolioBalanceSnapshot> =
     Map::new("history_portfolio_balance_snapshot_3");
+
+pub const DELETE_OLD_DATA_ENABLED: Item<bool> = Item::new("delete_old_data_enabled");
