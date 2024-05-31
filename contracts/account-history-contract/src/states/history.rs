3df2037688632091a9394ae0bfd94201430b9b1e
Amit Yadav <amy29981@gmail.com>
2024-05-31 10:20:24 +0530

    Fix: Update old map with old DS type (#358)
    update old map with old DS

--- contracts/account-history-contract/src/states/history.rs
+++ contracts/account-history-contract/src/states/history.rs
@@ -1,10 +1,12 @@
+use std::collections::HashMap;
+
 use cw_storage_plus::{Item, Map};
 use elys_bindings::account_history::types::PortfolioBalanceSnapshot;
 
-pub const OLD_HISTORY_1: Map<&str, PortfolioBalanceSnapshot> =
+pub const OLD_HISTORY_1: Map<&str, HashMap<String, PortfolioBalanceSnapshot>> =
     Map::new("history_portfolio_balance_snapshot_1");
 
-pub const OLD_HISTORY_2: Map<&str, PortfolioBalanceSnapshot> =
+pub const OLD_HISTORY_2: Map<&str, HashMap<String, PortfolioBalanceSnapshot>> =
     Map::new("history_portfolio_balance_snapshot_2");
 
 pub const HISTORY: Map<&str, PortfolioBalanceSnapshot> =
