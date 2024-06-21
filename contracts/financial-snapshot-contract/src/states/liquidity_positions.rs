6104784836aaac05278a984af78546e0fc5630fd
politeWall <138504353+politeWall@users.noreply.github.com>
2024-06-21 16:48:34 +0200

    feat: 🎸 Remove GetPodLiquidityPosition query

--- contracts/financial-snapshot-contract/src/states/liquidity_positions.rs
+++
@@ -1,4 +0,0 @@
-use crate::types::LiquidityPosition;
-use cw_storage_plus::Item;
-
-pub const LIQUIDITY_POSITIONS: Item<Vec<LiquidityPosition>> = Item::new("liquid_positions");
--- contracts/financial-snapshot-contract/src/tests/mock/instantiate.rs
+++ contracts/financial-snapshot-contract/src/tests/mock/instantiate.rs
@@ -2,8 +2,6 @@ use cosmwasm_schema::cw_serde;
 use cosmwasm_std::{DepsMut, Env, MessageInfo, Response, StdResult};
 use elys_bindings::{ElysMsg, ElysQuery};
 
-use crate::states::*;
-
 #[cw_serde]
 pub struct InstantiateMockMsg {
     pub epoch_cycle_interval: u128,
