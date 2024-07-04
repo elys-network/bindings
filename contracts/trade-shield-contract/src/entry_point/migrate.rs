674708e69d7f44d18a79fa19a8b37a576e26a24c
Servio <servio.za@gmail.com>
2024-07-04 17:57:28 -0400

    self versioning was added

--- contracts/trade-shield-contract/src/entry_point/migrate.rs
+++ contracts/trade-shield-contract/src/entry_point/migrate.rs
@@ -1,3 +1,8 @@
+use std::{
+    f32::consts::E,
+    fmt::{format, Debug},
+};
+
 use self::instantiate::{CONTRACT_NAME, CONTRACT_VERSION};
 
 use super::*;
@@ -11,6 +16,7 @@ use elys_bindings::trade_shield::{
         SWAP_ENABLED,
     },
 };
+use semver::Version;
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn migrate(
@@ -50,10 +56,18 @@ pub fn migrate(
     if ver.contract != CONTRACT_NAME {
         return Err(StdError::generic_err("Can only upgrade from same type").into());
     }
-    if ver.version.as_str() >= CONTRACT_VERSION {
-        return Err(StdError::generic_err("Cannot upgrade from a newer version").into());
-    }
 
+    let new_contract_version = Version::parse(CONTRACT_VERSION).unwrap();
+    let actual_contract_version = Version::parse(ver.version.as_str()).unwrap();
+
+    if new_contract_version.le(&actual_contract_version) {
+        let err_version: String = format!(
+            "Error the version of trade-shield-contract {} has to be upper to {}",
+            new_contract_version.to_string(),
+            actual_contract_version.to_string()
+        );
+        return Err(StdError::generic_err(err_version).into());
+    }
     // set the new version
     set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
     Ok(Response::new())
